005f75d0e38dfdb4549451d68632acee0d044903
lopeslyra10 <augustolopeslyra@gmail.com>
2024-10-25 17:57:30 -0300

    Atualizando o rodapé

--- cp6-front/src/app/Components/Rodape/Rodape.tsx
+++ cp6-front/src/app/Components/Rodape/Rodape.tsx
@@ -1,22 +1,8 @@
-import React from 'react';
-import Link from 'next/link';
-
-const Footer: React.FC = () => {
+export default function Rodape() {
   return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto text-center">
-        <p className="mb-4">&copy; 2024 Minha Aplicação. Todos os direitos reservados.</p>
-        <nav className="space-x-4">
-          <Link href="/privacy-policy">
-            <a className="hover:text-gray-400">Política de Privacidade</a>
-          </Link>
-          <Link href="/terms">
-            <a className="hover:text-gray-400">Termos de Serviço</a>
-          </Link>
-        </nav>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+      <footer>
+          <h2>Rodape</h2>
+          <p>Desenvolvido por <span>Rei Luizinho</span></p>
+      </footer>
+  )
+}
