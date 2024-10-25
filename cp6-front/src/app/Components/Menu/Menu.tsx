d348d0f74005925e9ccfc248c78d677c77f5d8d8
Coutinh00 <lucasconsentino57@gmail.com>
2024-10-25 20:15:27 -0300

    Menu atualizado

--- cp6-front/src/app/Components/Menu/Menu.tsx
+++ cp6-front/src/app/Components/Menu/Menu.tsx
@@ -6,9 +6,9 @@ export default function Menu() {
             <ul>
                 <li> <Link href="/">Home</Link> </li>
                 <li> <Link href="/Integrantes">Integrantes</Link> </li>
-                <li> <Link href="/Integrantes">Challenger Sprints</Link> </li>
-                <li> <Link href="/Integrantes">Global Solution</Link> </li>
-                <li> <Link href="/Integrantes">Challenger Sprints</Link> </li>
+                <li> <Link href="/avaliacoes/ChallengerSprints">Challenger Sprints</Link> </li>
+                <li> <Link href="/avaliacoes/GlobalSolution">Global Solution</Link> </li>
+                <li> <Link href="/avaliacoes/CheckPoints">CheckPoints</Link> </li>
             </ul>
         </nav>
     )
